949112e2d4d446856d1e36b92910d5265335fa25
Uneld <aleksey.minchenya@gmail.com>
2023-04-22 15:30:05 +0300

    added fourth solution

--- Task/Program.cs
+++ Task/Program.cs
@@ -68,6 +68,43 @@ string[] ReleaseArray3(string[] array)
     return outputArray;
 }
 
+
+string[] ReleaseArray4(ref string[] array, int length)
+{
+    //если дошли до длинны 1 проверяем последний кусок массива
+    if (length == 1)
+    {
+        // если подошло возврат текущего
+        if (array[length - 1].Length <= MAX_SIZE_STRING)
+        {
+            return new string[] { array[length - 1] };
+        }
+        // возврат пустого
+        return new string[0];
+    }
+
+    // забираем то что вернула функция для lenght - 1
+    string[] tempArray = ReleaseArray4(ref array, length - 1);
+
+    /*
+        проверяем если текущий элемент подходит под условие 
+        то копируем в новый массив предыдущий возврат и добавляем текущий элемент в конец 
+    */
+    if (array[length - 1].Length <= MAX_SIZE_STRING)
+    {
+        string[] outputArray = new string[tempArray.Length + 1];
+        for (int i = 0; i < tempArray.Length; i++)
+        {
+            outputArray[i] = tempArray[i];
+        }
+        outputArray[tempArray.Length] = array[length - 1];
+        return outputArray;
+    }
+    //если не подошло просто вовращаем предыдущий массив
+    return tempArray;
+}
+
+
 System.Console.Clear();
 System.Console.WriteLine("Please enter array");
 string[] inputArray = SingleLineInput(MAX_SIZE_INPUT_ARRAY);
@@ -85,4 +122,8 @@ PrintArrayString(releaseArray);
 
 System.Console.WriteLine("Way 3");
 releaseArray = ReleaseArray3(inputArray);
+PrintArrayString(releaseArray);
+
+System.Console.WriteLine("Way 4");
+releaseArray = ReleaseArray4(ref inputArray, inputArray.Length);
 PrintArrayString(releaseArray);
